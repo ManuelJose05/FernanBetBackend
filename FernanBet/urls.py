31b65258179864f1f14768a7cb1629940a56e3a8
ManuelJose05 <150932456+ManuelJose05@users.noreply.github.com>
2025-06-11 19:42:29 +0200

    Update urls.py

--- FernanBet/urls.py
+++ FernanBet/urls.py
@@ -15,11 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.contrib import admin
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from applications import users
+schema_view = get_schema_view(
+   openapi.Info(
+      title="FernandBet3 API",
+      default_version='v1',
+      description="Documentación API para FernandBet3",
+      contact=openapi.Contact(email="manuel.liebana.2001@fernando3martos.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +43,8 @@ urlpatterns = [
     path('api/v1/players/',include('applications.player.urls'),name='players'),
     path('api/v1/matchs/',include('applications.match.urls'),name='match'),
     path('api/v1/bets/', include('applications.apuestaUsuario.urls'), name='apuestas'),
+# Swagger UI:
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
